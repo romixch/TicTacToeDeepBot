1950ff76d1e647f14c0602b70f681452ede0f509
Roman Schaller <roman.schaller@gmail.com>
2017-08-06 16:25:34 +0200

    Persist games and rewards to a pickle file

--- Practice.py
+++ Practice.py
@@ -6,6 +6,7 @@ import TicTacToe as ttt
 import model_helper
 import numpy
 import KIPlayer
+import pickle
 
 # Parameters
 board_size = 5
@@ -13,8 +14,8 @@ runlength = 4
 exploration = 0.3
 learning_rate = 1e-4
 hidden_layers = 2
-hidden_layer_size_factor = 3
-games_to_play = 10000
+hidden_layer_size_factor = 2
+games_to_play = 2000
 reward_discount = 0.7
 punishment_discount = 0.7
 reward = 1.0
@@ -65,86 +66,95 @@ init = tf.global_variables_initializer()
 
 # Launch the graph
 with tf.Session() as sessX:
-    with tf.Session() as sessO:
-        sessX.run(init)
-        sessO.run(init)
-        writer = new_tensor_file_writer()
-        writer.add_graph(sessX.graph)
-        writer.add_graph(sessO.graph)
-        tf.summary.scalar("cost", cost)
-        tf.summary.scalar('x_wins', x_wins)
-        tf.summary.scalar('o_wins', o_wins)
-        tf.summary.scalar('draw', draw)
-        summary_op = tf.summary.merge_all()
+    with open('learn_x.dat', 'wb') as learn_x_file:
+        with tf.Session() as sessO:
+            with open('learn_o.dat', 'wb') as learn_o_file:
+                sessX.run(init)
+                sessO.run(init)
+                writer = new_tensor_file_writer()
+                writer.add_graph(sessX.graph)
+                writer.add_graph(sessO.graph)
+                tf.summary.scalar("cost", cost)
+                tf.summary.scalar('x_wins', x_wins)
+                tf.summary.scalar('o_wins', o_wins)
+                tf.summary.scalar('draw', draw)
+                summary_op = tf.summary.merge_all()
 
-        for game_index in range(games_to_play):
-            # Play a full game
-            game = ttt.TicTacToe(board_size, board_size, runlength)
-            players = [game.playerX, game.playerO]
-            playerId = 0
-            states = {
-                game.playerX: numpy.empty((0, 0)),
-                game.playerO: numpy.empty((0, 0))
-            }
-            actions = {
-                game.playerX: [],
-                game.playerO: []
-            }
-            while not game.isFinished():
-                player = players[playerId]
+                for game_index in range(games_to_play):
+                    # Play a full game
+                    game = ttt.TicTacToe(board_size, board_size, runlength)
+                    players = [game.playerX, game.playerO]
+                    playerId = 0
+                    states = {
+                        game.playerX: numpy.empty((0, 0)),
+                        game.playerO: numpy.empty((0, 0))
+                    }
+                    actions = {
+                        game.playerX: [],
+                        game.playerO: []
+                    }
+                    while not game.isFinished():
+                        player = players[playerId]
 
-                # Record board state
-                states[player] = numpy.append(states[player], game.board_for_learning())
+                        # Record board state
+                        states[player] = numpy.append(states[player], game.board_for_learning())
 
-                # Calculate next move
-                if player == game.playerX:
-                    idx_x, idx_y = KIPlayer.next_move(game, sessX, pred, x, exploration)
-                else:
-                    idx_x, idx_y = KIPlayer.next_move(game, sessO, pred, x, exploration)
+                        # Calculate next move
+                        if player == game.playerX:
+                            idx_x, idx_y = KIPlayer.next_move(game, sessX, pred, x, exploration)
+                        else:
+                            idx_x, idx_y = KIPlayer.next_move(game, sessO, pred, x, exploration)
 
-                # set next move
-                game.setField(idx_x, idx_y, player)
+                        # set next move
+                        game.setField(idx_x, idx_y, player)
 
-                # record played move
-                field = idx_y * game.board_width + idx_x
-                actions[player] = numpy.append(actions[player], field)
+                        # record played move
+                        field = idx_y * game.board_width + idx_x
+                        actions[player] = numpy.append(actions[player], field)
 
-                # move to next player
-                playerId = (playerId + 1) % 2
+                        # move to next player
+                        playerId = (playerId + 1) % 2
 
-                # Print every one or other game
-                if game_index % 500 == 0:
-                    print(game.get_pretty_board)
+                        # Print every one or other game
+                        if game_index % 500 == 0:
+                            print(game.get_pretty_board)
 
-            # reshape numpy arrays
-            states[game.playerX] = reshape(states[game.playerX], game.board_field_size)
-            states[game.playerO] = reshape(states[game.playerO], game.board_field_size)
+                    # reshape numpy arrays
+                    states[game.playerX] = reshape(states[game.playerX], game.board_field_size)
+                    states[game.playerO] = reshape(states[game.playerO], game.board_field_size)
 
-            # Calculate discounted rewards for learning
-            winner = game.playerX if game.isWinnerX() else game.playerO
-            rewards_winner = calculate_discounted_rewards(states[winner], actions[winner], reward)
+                    # Calculate discounted rewards for learning
+                    is_winner_x = game.isWinnerX()
+                    is_winner_o = game.isWinnerO()
+                    winner = game.playerX if is_winner_x else game.playerO
+                    rewards_winner = calculate_discounted_rewards(states[winner], actions[winner], reward)
 
-            looser = game.playerO if game.isWinnerX() else game.playerX
-            punishments_looser = calculate_discounted_rewards(states[looser], actions[looser], punishment)
+                    looser = game.playerO if is_winner_x else game.playerX
+                    punishments_looser = calculate_discounted_rewards(states[looser], actions[looser], punishment)
 
-            # Learn from past game as winner
-            x_wins_float = 1.0 if game.isWinnerX() else 0.0
-            o_wins_float = 1.0 if game.isWinnerO() else 0.0
-            draw_float = 1.0 if not game.isWinnerX() and not game.isWinnerO() else 0.0
-            sess_winner = sessX if game.isWinnerX() else sessO
-            _, summary = sess_winner.run([optimizer, summary_op], feed_dict={x: states[winner], y: rewards_winner, x_wins: x_wins_float, o_wins: o_wins_float, draw: draw_float})
-            writer.add_summary(summary, game_index)
+                    # Learn from past game as winner
+                    x_wins_float = 1.0 if is_winner_x else 0.0
+                    o_wins_float = 1.0 if is_winner_o else 0.0
+                    draw_float = 1.0 if not is_winner_x and not is_winner_o else 0.0
+                    sess_winner = sessX if is_winner_x else sessO
+                    _, summary = sess_winner.run([optimizer, summary_op], feed_dict={x: states[winner], y: rewards_winner, x_wins: x_wins_float, o_wins: o_wins_float, draw: draw_float})
+                    writer.add_summary(summary, game_index)
 
-            # Learn from past game as looser
-            x_wins_array = 1.0 if game.isWinnerX() else 0.0
-            o_wins_float = 1.0 if game.isWinnerO() else 0.0
-            draw_float = 1.0 if not game.isWinnerX() and not game.isWinnerO() else 0.0
-            sess_looser = sessO if game.isWinnerX() else sessX
-            _, summary = sess_looser.run([optimizer, summary_op], feed_dict={x: states[looser], y: punishments_looser, x_wins: x_wins_float, o_wins: o_wins_float, draw: draw_float})
-            writer.add_summary(summary, game_index)
+                    # Learn from past game as looser
+                    x_wins_array = 1.0 if is_winner_x else 0.0
+                    o_wins_float = 1.0 if is_winner_o else 0.0
+                    draw_float = 1.0 if not is_winner_x and not is_winner_o else 0.0
+                    sess_looser = sessO if is_winner_x else sessX
+                    _, summary = sess_looser.run([optimizer, summary_op], feed_dict={x: states[looser], y: punishments_looser, x_wins: x_wins_float, o_wins: o_wins_float, draw: draw_float})
+                    writer.add_summary(summary, game_index)
 
-            if game_index % int(games_to_play / 100) == 0:
-                print('finished', int(game_index / games_to_play * 100), '%')
+                    pickle.dump(states[game.playerX], learn_x_file)
+                    pickle.dump(states[game.playerO], learn_o_file)
+                    pickle.dump(rewards_winner, learn_x_file) if is_winner_x else pickle.dump(punishments_looser, learn_o_file)
+                    pickle.dump(punishments_looser, learn_o_file) if is_winner_x else pickle.dump(punishments_looser, learn_x_file)
 
-        model_helper.save_model(sessO, 'tf-model/O', x, pred)
+                    if game_index % int(games_to_play / 100) == 0:
+                        print('finished', int(game_index / games_to_play * 100), '%')
+
+            model_helper.save_model(sessO, 'tf-model/O', x, pred)
     model_helper.save_model(sessX, 'tf-model/X', x, pred)
